7fee1a8bb39338f9bc2476b5038485c7861e487b
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-04 22:38:51 +0100

    Add attack monster code

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return {
+        playerHealth: 100,
+        monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+        const attackValue = Math.floor(Math.random() * (12 - 5)) + 5;
+        this.monsterHealth -= attackValue;
+    }
+  }
+});
+
+app.mount("#game");
