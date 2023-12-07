fba95d6ae98c81f7109ad1363131c3fc9e07b1ef
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-08 00:13:25 +0100

    Add disable special attack logic

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
@@ -19,6 +20,7 @@ const app = Vue.createApp({
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -28,6 +30,7 @@ const app = Vue.createApp({
       this.playerHealth -= attackValue;
     },
     specialAttackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
